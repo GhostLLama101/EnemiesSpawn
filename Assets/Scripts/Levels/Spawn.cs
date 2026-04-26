c9ca8bc5095b86404ff4ab999a23587fce368bf2
Joseph Dadlez <jsdadlez@outlook.com>
2026-04-25 22:23:47 -0700

    Got the delay to work

--- Assets/Scripts/Levels/Spawn.cs
+++ Assets/Scripts/Levels/Spawn.cs
@@ -7,7 +7,7 @@ public class Spawn
     public string enemy;
     public string count;
     public string hp;
-    public string delay;
+    public int delay;
     public List<int> intsequence;
     public string location;
 }
