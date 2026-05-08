5734b1db25caeda20cdd5656b08c481ada1e942f
Joseph Dadlez <jsdadlez@outlook.com>
2026-05-08 00:48:16 -0700

    Made the base spell

--- Assets/Scripts/Spells/Spell.cs
+++ Assets/Scripts/Spells/Spell.cs
@@ -65,6 +65,7 @@ public class Spell
         GameManager.Instance.projectileManager.CreateProjectile(0, "straight", where, target - where, 15f, OnHit);
         yield return new WaitForEndOfFrame();
     }
+    // need to make a cast that takes in a 
     
     
 
