0eab63ca9817c391ce534e4b6cc37ee144fe464a
ZanderZ15 <zandermillerbrown@gmail.com>
2026-05-11 23:07:22 -0700

    Added the homing modifier; needs SetAttributes to be updated

--- Assets/Scripts/Spells/ModTypes/homing.cs
+++ Assets/Scripts/Spells/ModTypes/homing.cs
@@ -1,16 +1,28 @@
 using UnityEngine;
 using Newtonsoft.Json.Linq;
+using Unity.VisualScripting;
+using System.Collections.Generic;
+using static RPNEvaluator.RPNEvaluator;
 public class homing : Modifier
 {
+
+    
+    
+
     public homing(SpellCaster owner, ModifierInfo spell, Spell inner) : base(owner, spell, inner)
     {
-        
+        ModifierInfo.name = "homing";
+        ModifierInfo.description = 
+        "The spell's projectile now homes in on the nearest target. Damage is decreased. Mana cost is increased.";
+        ModifierInfo.damage_multiplier = "0.9";
+        ModifierInfo.mana_adder = "5";
+        ModifierInfo.projectile_trajectory = "homing";
     }
-    //TODO: Actually make this set the attributes
     public override void SetAttributes(JObject mod)
     {
         base.SetAttributes(mod);
-        //multiply mana cost by x1.25
+        //add 5 to mana cost
+        //decrease mana cost by 10%
         //make tragectory of projectile homing
         //this.ModifierInfo.damage_multiplier = mod["damage_multiplier"].ToString();
         this.ModifierInfo.mana_multiplier = mod["mana_multiplier"].ToString();
