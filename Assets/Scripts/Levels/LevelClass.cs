e774cf911d1a8c286f005696a917bc5a24962d42
ZanderZ15 <zandermillerbrown@gmail.com>
2026-05-29 19:10:41 -0700

    split up level and spawns

--- Assets/Scripts/Levels/LevelClass.cs
+++ Assets/Scripts/Levels/LevelClass.cs
@@ -8,16 +8,3 @@ public class LevelClass
     public int waves = 5;
     public List<Spawns> spawns;
 }
-[System.Serializable]
-public class Spawns
-{
-    public string enemy = "zombie";
-    public string count = "1";
-    public string hp = "base";
-    public string speed = "base";
-    public string damage = "base";
-    public string delay = "2";
-    public int[] sequence = {1};
-    public string location = "random";
-
-}
