f660f3e881017d02920b7f05507fa610ae35bdf4
Sai <saahamid@ucsc.edu>
2026-05-31 22:23:26 -0700

    drafts of whirlpool and fireamp edited

--- Assets/Scripts/Spells/ModTypes/firerateAmp.cs
+++ Assets/Scripts/Spells/ModTypes/firerateAmp.cs
@@ -9,25 +9,28 @@ public class firerateAmp : Modifier
     {
         
     }
-    public override void SetAttributes(JObject attributes) {
-        base.SetAttributes(attributes);
-        ModifierInfo.angle = attributes["angle"].ToString();
-        ModifierInfo.mana_multiplier = attributes["mana_multiplier"].ToString();
-      
+    public override void SetAttributes(JObject mod)
+    {
+        base.SetAttributes(mod);
+
+        this.ModifierInfo.damage_multiplier = mod["damage_multiplier"].ToString();
+        this.ModifierInfo.cooldown_multiplier = mod["cooldown_multiplier"].ToString();
+    }
+
+
+    public override void ApplyModStats()
+    {
+        this.ModifierInfo.damage_multiplier = "0.75";
+        this.ModifierInfo.cooldown_multiplier = "0.5";
+
+        inner.spellInfo.damage.amount += this.ModifierInfo.damage_multiplier;
+        inner.spellInfo.cooldown += this.ModifierInfo.cooldown_multiplier;
+
     }
 
     public override IEnumerator Cast(Vector3 where, Vector3 target, Hittable.Team team)
     {
         this.team = team;
-        Vector3 direction = (target - where).normalized;
-        int angle = int.Parse(ModifierInfo.angle);
-        
-        Vector3 target1 = where + (Quaternion.Euler(0, 0, angle) * direction);
-        Vector3 target2 = where + (Quaternion.Euler(0, 0, -angle) * direction);
-        
-        GameManager.Instance.projectileManager.StartCoroutine(inner.Cast(where, target1, team));
-        GameManager.Instance.projectileManager.StartCoroutine(inner.Cast(where, target2, team));
-
-        yield break;
+        yield return inner.Cast(where, target, team);
     }
 }
