d8273f1cab9cc8651c8c0dd80771479f538d1faa
JohnnyFisher27 <157189363+JohnnyFisher27@users.noreply.github.com>
2026-06-09 00:41:41 -0700

    change class name

--- Assets/Scripts/SkillTreeCode/ReadSkillTree.cs
+++ Assets/Scripts/SkillTreeCode/ReadSkillTree.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 
-public class ReadSpellSlot : MonoBehaviour
+public class ReadSkillTree : MonoBehaviour
 {
     public List<SpellSlot> spellSlots;
     public Modifier Modifiers;
